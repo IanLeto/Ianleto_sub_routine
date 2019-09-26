662215311b4ae35c0e45003a05694afaa85ad6e5
lan <lan@canway.net>
2019-09-26 10:26:42 +0800

    start again and again

--- r1.py
+++ r1.py
@@ -7,17 +7,18 @@
 
 from collections import Counter
 
+
 def fib(n, counter):
-	counter.increment()
-	if n < 3:
-		return 1
-	else:
-		return fib(n-1,counter) + fib(n-2, counter)
+    counter.increment()
+    if n < 3:
+        return 1
+    else:
+        return fib(n - 1, counter) + fib(n - 2, counter)
+
+
 Size = 2
-print('%s,%s' %("Size", "Counter"))
-for i in range(1,5):
-	count = Counter()
-	fib(i, count)
-	print ('%d,%d' % (i,count))
-
-
+print('%s,%s' % ("Size", "Counter"))
+for i in range(1, 5):
+    count = Counter()
+    fib(i, count)
+    print('%d,%d' % (i, count))
